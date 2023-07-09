65d4430ff223dbde11a534da50aabbc90e64ff0c
Dominic Fellbaum <d.fellbaum@hotmail.de>
2023-07-09 03:20:09 +0200

    Add benchmark tests

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -29,6 +29,9 @@ plugins {
     `maven-publish`
     signing
     id("com.github.ben-manes.versions") version "0.47.0"
+
+    //https://github.com/melix/jmh-gradle-plugin
+    id("me.champeau.jmh") version "0.7.1"
 }
 
 repositories {
@@ -47,6 +50,45 @@ testing {
             useJUnit("4.13.2")
         }
     }
+}
+
+jmh {
+//    includes.addAll("some regular expression")  // include pattern (regular expression) for benchmarks to be executed
+//    excludes.addAll("some regular expression") // exclude pattern (regular expression) for benchmarks to be executed
+//    iterations.set(10) // Number of measurement iterations to do.
+    benchmarkMode.addAll("avgt") // Benchmark mode. Available modes are: [Throughput/thrpt, AverageTime/avgt, SampleTime/sample, SingleShotTime/ss, All/all]
+//    batchSize.set(1) // Batch size: number of benchmark method calls per operation. (some benchmark modes can ignore this setting)
+    fork.set(2) // How many times to forks a single benchmark. Use 0 to disable forking altogether
+//    failOnError.set(false) // Should JMH fail immediately if any benchmark had experienced the unrecoverable error?
+//    forceGC.set(false) // Should JMH force GC between iterations?
+//    jvm.set("myjvm") // Custom JVM to use when forking.
+//    jvmArgs.addAll("Custom JVM args to use when forking.")
+//    jvmArgsAppend.addAll("Custom JVM args to use when forking (append these)")
+//    jvmArgsPrepend.addAll("Custom JVM args to use when forking (prepend these)")
+//    humanOutputFile.set(project.file("${project.buildDir}/reports/jmh/human.txt")) // human-readable output file
+//    resultsFile.set(project.file("${project.buildDir}/reports/jmh/results.txt")) // results file
+//    operationsPerInvocation.set(10) // Operations per invocation.
+//    //benchmarkParameters.put("", ListProperty) // Benchmark parameters.
+//    profilers.addAll("") // Use profilers to collect additional data. Supported profilers: [cl, comp, gc, stack, perf, perfnorm, perfasm, xperf, xperfasm, hs_cl, hs_comp, hs_gc, hs_rt, hs_thr, async]
+//    timeOnIteration.set("1s") // Time to spend at each measurement iteration.
+//    resultFormat.set("CSV") // Result format type (one of CSV, JSON, NONE, SCSV, TEXT)
+//    synchronizeIterations.set(false) // Synchronize iterations?
+//    threads.set(4) // Number of worker threads to run with.
+//    threadGroups.addAll(2,3,4) //Override thread group distribution for asymmetric benchmarks.
+//    jmhTimeout.set("1s") // Timeout for benchmark iteration.
+    timeUnit.set("ms") // Output time unit. Available time units are: [m, s, ms, us, ns].
+//    verbosity.set("NORMAL") // Verbosity mode. Available modes are: [SILENT, NORMAL, EXTRA]
+//    warmup.set("1s") // Time to spend at each warmup iteration.
+//    warmupBatchSize.set(10) // Warmup batch size: number of benchmark method calls per operation.
+//    warmupForks.set(0) // How many warmup forks to make for a single benchmark. 0 to disable warmup forks.
+//    warmupIterations.set(1) // Number of warmup iterations to do.
+//    warmupMode.set("INDI") // Warmup mode for warming up selected benchmarks. Warmup modes are: [INDI, BULK, BULK_INDI].
+//    warmupBenchmarks.addAll(".*Warmup") // Warmup benchmarks to include in the run in addition to already selected. JMH will not measure these benchmarks, but only use them for the warmup.
+//
+//    zip64.set(true) // Use ZIP64 format for bigger archives
+//    jmhVersion.set("1.36") // Specifies JMH version
+//    includeTests.set(true) // Allows to include test sources into generate JMH jar, i.e. use it when benchmarks depend on the test classes.
+//    duplicateClassesStrategy.set(DuplicatesStrategy.FAIL) // Strategy to apply when encountring duplicate classes during creation of the fat jar (i.e. while executing jmhJar task)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
---
+++ lib/src/jmh/java/benchmark/BenchmarkRunner.java
@@ -0,0 +1,68 @@
+package benchmark;
+
+import net.fellbaum.jemoji.Emoji;
+import net.fellbaum.jemoji.EmojiManager;
+import net.fellbaum.jemoji.EmojiManagerTest;
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class BenchmarkRunner {
+
+    private static final String TEXT = new BufferedReader(new InputStreamReader(Objects.requireNonNull(BenchmarkRunner.class.getClassLoader().getResourceAsStream("ExampleTextFileWithEmojis.txt"))))
+            .lines().collect(Collectors.joining("\n"));
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+
+    private static final Collector<?, ?, ?> SHUFFLER = Collectors.collectingAndThen(
+            Collectors.toCollection(ArrayList::new),
+            list -> {
+                Collections.shuffle(list);
+                return list;
+            }
+    );
+
+    @SuppressWarnings("unchecked")
+    public static <T> Collector<T, ?, List<T>> toShuffledList() {
+        return (Collector<T, ?, List<T>>) SHUFFLER;
+    }
+
+    private static final String EMOJIS_RANDOM_ORDER = String.join("", EmojiManager.getAllEmojisLengthDescending().stream().map(Emoji::getEmoji).collect(toShuffledList()));
+
+
+    @Benchmark
+    //@BenchmarkMode(Mode.AverageTime)
+    public String replaceAllEmojis() {
+        return EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
+    }
+
+    @Benchmark
+    public String removeAllEmojis() {
+        return EmojiManager.removeAllEmojis(TEXT);
+    }
+
+    @Benchmark
+    public List<Emoji> extractEmojisInOrder() {
+        return EmojiManager.extractEmojisInOrder(TEXT);
+    }
+
+    @Benchmark
+    public List<Emoji> extractEmojisInOrderOnlyEmojisLengthDescending() {
+        return EmojiManager.extractEmojisInOrder(EmojiManagerTest.ALL_EMOJIS_STRING);
+    }
+
+    @Benchmark
+    public List<Emoji> extractEmojisInOrderOnlyEmojisRandomOrder() {
+        return EmojiManager.extractEmojisInOrder(EMOJIS_RANDOM_ORDER);
+    }
+
+}
