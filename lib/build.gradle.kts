7649780595ead148daf0a2de0a1dad5cd459a16e
Dessie <42523509+Dessie0@users.noreply.github.com>
2023-07-11 13:21:31 -0400

    Set encoding options in build.gradle.kts (#2)
    Set encoding to UTF-8 in build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -100,6 +100,18 @@ java {
     }
 }
 
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    systemProperty("file.encoding", "UTF-8")
+}
+
+tasks.withType<Javadoc>{
+    options.encoding = "UTF-8"
+}
+
 tasks.named("build") {
     finalizedBy("copyJarToProject")
 }
