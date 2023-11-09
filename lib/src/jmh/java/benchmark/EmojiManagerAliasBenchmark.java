bcf521a3a34ed36519179cef6352565368846479
freya02 <41875020+freya022@users.noreply.github.com>
2023-11-09 16:29:02 +0100

    Arrange benchmarks to avoid using unnecessary parameters

---
+++ lib/src/jmh/java/benchmark/EmojiManagerAliasBenchmark.java
@@ -0,0 +1,22 @@
+package benchmark;
+
+import net.fellbaum.jemoji.Emoji;
+import net.fellbaum.jemoji.EmojiManager;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.Optional;
+
+@State(Scope.Benchmark)
+public class EmojiManagerAliasBenchmark {
+
+    @Param({":+1:", "nope"})
+    private String alias;
+
+    @Benchmark
+    public Optional<Emoji> getByAlias() {
+        return EmojiManager.getByAlias(alias);
+    }
+}
