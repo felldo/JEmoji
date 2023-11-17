00846fa5e909f916c3e39bfeb9dcacdd3300237e
github-action-benchmark <github@users.noreply.github.com>
2023-11-17 13:43:07 +0000

    add Benchmark (jmh) benchmark result for 52e0e837296f201da105590f998096c808647023

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1700228587475,
+  "repoUrl": "https://github.com/felldo/JEmoji",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "felldo",
+            "username": "felldo"
+          },
+          "committer": {
+            "name": "felldo",
+            "username": "felldo"
+          },
+          "id": "52e0e837296f201da105590f998096c808647023",
+          "message": "java",
+          "timestamp": "2023-11-05T10:05:19Z",
+          "url": "https://github.com/felldo/JEmoji/pull/35/commits/52e0e837296f201da105590f998096c808647023"
+        },
+        "date": 1700228586745,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "benchmark.EmojiManagerBenchmark.replaceAllEmojis",
+            "value": 32.28505938699597,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 2\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
