e06648130f8ac24fda021380da43b0691f36466f
Dominic Fellbaum <d.fellbaum@hotmail.de>
2023-11-18 16:09:59 +0100

    test

--- lib/src/jmh/java/benchmark/EmojiManagerBenchmark.java
+++ lib/src/jmh/java/benchmark/EmojiManagerBenchmark.java
@@ -48,6 +48,13 @@ public class EmojiManagerBenchmark {
     //@BenchmarkMode(Mode.AverageTime)
     //@Warmup(iterations = 1)
     public String replaceAllEmojis() {
+        EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
+        EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
+        EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
+        EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
+        EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
+        EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
+        EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
         return EmojiManager.replaceAllEmojis(TEXT, "<replaced emoji>");
     }
 
